897d8f33d3113416e579aee13652df04a211aa8b
Lindsay <linmalex@gmail.com>
2019-05-15 21:33:56 +0000

    test root controller change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello_world
-      render html: "Hello world"
+      render html: "Hello new person"
   end
 end
